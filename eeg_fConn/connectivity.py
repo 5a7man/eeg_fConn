ceb4f2909a36c36d209f08c9b0d1c373a930e395
Muhammad Salman Kabir <58083322+5a7man@users.noreply.github.com>
2023-10-14 17:19:33 +0300

    Added icoh_connectivity
    added function "icoh_connectivity()" to compute imaginary part of coherency

--- eeg_fConn/connectivity.py
+++ eeg_fConn/connectivity.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 27 22:53:29 2022
-
 @author: Muhammad Salman Kabir
-@purpose: Functions to do band pass filteration andcomputing connectivity matrix
-          and/or connectivity vector
+@purpose: Functions to do band pass filteration and computing connectivity matrix
+          and connectivity vector
 @regarding: Functional connectivity anaylsis    
 """
 
@@ -14,15 +12,17 @@ import scipy.signal as ss
 
 def filteration(data,f_min,f_max,fs):
     """
+    Performing band pass filteration
+    
     Parameters
     ----------
     data : Array of float
         DESCRIPTION. EEG data
     f_min : float
         DESCRIPTION. Low pass frequency of band pass filter given in hertz
-    f_max : TYPE: float
+    f_max : float
         DESCRIPTION. High pass frequency of band pass filter given in hertz
-    fs : TYPE: float
+    fs : float
         DESCRIPTION. Sampling frequency of data given in hertz
 
     Returns 
@@ -33,17 +33,19 @@ def filteration(data,f_min,f_max,fs):
     """
     print("Filteration in process.....")
     
-    # filter design
+    # Filter design
     sos = ss.butter(N=10,Wn=[f_min,f_max],btype='bandpass',
                     analog=False,output='sos',fs=fs)
 
-    # returning filter data
+    # Returning filtered data
     print("Filteration done!")
     return ss.sosfilt(sos,data)
 
 
 def plv_connectivity(sensors,data):
     """
+    Computing PLV connectivity
+    
     Parameters
     ----------
     sensors : INT
@@ -83,6 +85,8 @@ def plv_connectivity(sensors,data):
             
 def pli_connectivity(sensors,data):
     """
+    Computing PLI connectivity
+    
     Parameters
     ----------
     sensors : INT
@@ -122,6 +126,8 @@ def pli_connectivity(sensors,data):
 
 def ccf_connectivity(sensors,data):
     """
+    Computing Cross Correlation
+    
     Parameters
     ----------
     sensors : INT
@@ -133,7 +139,7 @@ def ccf_connectivity(sensors,data):
     -------
     connectivity_matrix : Matrix of float
         DESCRIPTION. CCF connectivity matrix
-    connectivity_vector : Vector of flaot 
+    connectivity_vector : Vector of float 
         DESCRIPTION. CCF connectivity vector
 
     """
@@ -158,18 +164,26 @@ def ccf_connectivity(sensors,data):
 
 def coh_connectivity(sensors,data,f_min,f_max,fs):
     """
+    Computing Coherence
+    
     Parameters
     ----------
     sensors : INT
         DESCRIPTION. No of sensors used for capturing EEG
     data : Array of float 
         DESCRIPTION. EEG Data
+    f_min : float
+        DESCRIPTION. Low pass frequency of band pass filter given in hertz
+    f_max : TYPE: float
+        DESCRIPTION. High pass frequency of band pass filter given in hertz
+    fs : TYPE: float
+        DESCRIPTION. Sampling frequency of data given in hertz
     
     Returns
     -------
     connectivity_matrix : Matrix of float
         DESCRIPTION. COH connectivity matrix
-    connectivity_vector : Vector of flaot 
+    connectivity_vector : Vector of float 
         DESCRIPTION. COH connectivity vector
 
     """
@@ -189,6 +203,52 @@ def coh_connectivity(sensors,data,f_min,f_max,fs):
     
     # returning connectivity matrix and/or vector
     print("COH done!")
+    return connectivity_matrix, connectivity_vector
+
+def icoh_connectivity(sensors,data,f_min,f_max,fs):
+    """
+    Computing imaginary Coherence
+    
+    Parameters
+    ----------
+    sensors : INT
+        DESCRIPTION. No of sensors used for capturing EEG
+    data : Array of float 
+        DESCRIPTION. EEG Data
+    f_min : float
+        DESCRIPTION. Low pass frequency of band pass filter given in hertz
+    f_max : TYPE: float
+        DESCRIPTION. High pass frequency of band pass filter given in hertz
+    fs : TYPE: float
+        DESCRIPTION. Sampling frequency of data given in hertz
+    
+    Returns
+    -------
+    connectivity_matrix : Matrix of float
+        DESCRIPTION. ICOH connectivity matrix
+    connectivity_vector : Vector of float 
+        DESCRIPTION. ICOH connectivity vector
+
+    """
+    print("ICOH in process.....")
+    
+    # Predefinig connectivity matrix
+    connectivity_matrix = np.zeros([sensors,sensors],dtype=float)
+    
+    # Computing imaginary coherence 
+    for i in range(sensors):
+        _, Pxx = ss.welch(data[i,:],fs=fs) 
+        for k in range(sensors):
+            _, Pyy = ss.welch(data[k,:],fs=fs) 
+            f, Pxy = ss.csd(data[i,:],data[k,:],fs=fs)
+            icoh = np.imag(Pxy)/(np.sqrt(Pxx*Pyy))
+            connectivity_matrix[i,k] = np.mean(icoh[np.where((f>=f_min) & (f<=f_max))])
+    
+    # Computing connectivity vector
+    connectivity_vector = connectivity_matrix[np.triu_indices(connectivity_matrix.shape[0],k=1)] 
+    
+    # returning connectivity matrix and/or vector
+    print("ICOH done!")
     return connectivity_matrix, connectivity_vector
 
             
